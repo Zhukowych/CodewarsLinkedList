3d1512f1f524642f6155c06ddc9cbd0e396291e3
Maksym Zhuk <mzmzhuk@gmail.com>
2024-04-04 15:20:26 +0300

    Added solution of Convert a linked list to a string problem

---
+++ Convert a linked list to a string/solution.py
@@ -0,0 +1,7 @@
+def stringify(node):
+    values = [] 
+    while node:
+        values.append(node.data)
+        node = node.next
+    values.append(None)
+    return ' -> '.join(map(str, values))*
